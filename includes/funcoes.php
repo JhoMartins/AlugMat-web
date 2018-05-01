bedb50b273f50bffb4b136e4d75649b621dd6bb4
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-05-01 14:55:39 -0300

    mudanças nas funcoes de logiin

--- includes/funcoes.php
+++ includes/funcoes.php
@@ -1,5 +1,5 @@
 <?php
-  function abosolute_url ($page = 'index.php') {
+  function absolute_url ($page = 'index.php') {
 	  $url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
 	  $url = rtrim($url, '/\\');
 	  $url .= '/' . $page;
@@ -16,21 +16,19 @@
 		$n = mysqli_real_escape_string($dbc, trim($nome));
 	  }
 
-	  if (empty($nome)) {
+	  if (empty($senha)) {
 	    $erros[] = 'Você esqueceu de digitar a sua Senha.';
 	  }
 	  else {
-	    $n = mysqli_real_escape_string($dbc, trim($senha));
+	    $s = mysqli_real_escape_string($dbc, trim($senha));
 	  }
 
 	  if (empty($erros)){
-		 $q = "SELECT id, nome, FROM cliente
-			   WHERE email = '$n' AND senha=SHA1('bd_alugmat.$s')";
+		$q = "SELECT id, nome, FROM cliente WHERE email = '$n' AND senha = SHA1('bd_alugmat.$s')";
 		$r = mysqli_query($dbc, $q);
 
-		if (mysqli_num_rows($r) == 1){
+		if (mysqli_num_rows($r) == 0){
 			$row = mysqli_fetch_array($r, MYSQLI_ASSOC);
-
 			return array(true, $row);
 		}
 		else {
