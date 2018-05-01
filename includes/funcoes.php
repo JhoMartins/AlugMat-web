df8390417a3a11fba34dec9a6ddc7f193d263079
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-05-01 13:02:54 -0300

    cria funções para auxiliar no login de usuários

--- includes/funcoes.php
+++ includes/funcoes.php
@@ -1,46 +1,42 @@
 <?php
-	function absolute_url ($page = 'menu_principal.php')
-	{
-		$url = 'http://' . $_SERVER['HTTP_HOST'] .
-		dirname($_SERVER['PHP_SELF']);
-		$url = rtrim($url, '/\\');
-		$url.='/'.$page;
-		return $url;
-	}
+  function abosolute_url ($page = 'index.php') {
+	  $url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
+	  $url = rtrim($url, '/\\');
+	  $url .= '/' . $page;
+	  return $url;
+  }
 
-	function check_login($dbc, $nome='', $senha='')
-	{
-		$erros = array();
-		if (empty($nome)) {
-			$erros[] = 'Você esqueceu de digitar o seu login.';
-		} else {
-			$n = mysqli_real_escape_string($dbc, trim($nome));
+  function check_login($dbc, $nome = '', $senha = ''){
+	  $erros = array();
+
+	  if (empty($nome)) {
+		  $erros[] = 'Você esqueceu de digitar o seu Login.';
+	  }
+	  else {
+		$n = mysqli_real_escape_string($dbc, trim($nome));
+	  }
+
+	  if (empty($nome)) {
+	    $erros[] = 'Você esqueceu de digitar a sua Senha.';
+	  }
+	  else {
+	    $n = mysqli_real_escape_string($dbc, trim($senha));
+	  }
+
+	  if (empty($erros)){
+		 $q = "SELECT id, nome, FROM cliente
+			   WHERE email = '$n' AND senha=SHA1('bd_alugmat.$s')";
+		$r = mysqli_query($dbc, $q);
+
+		if (mysqli_num_rows($r) == 1){
+			$row = mysqli_fetch_array($r, MYSQLI_ASSOC);
+
+			return array(true, $row);
 		}
-
-		if (empty($senha)) {
-			$erros[] = 'Você esqueceu de digitar a sua senha.';
-		} else {
-			$s = mysqli_real_escape_string($dbc, trim($senha));
+		else {
+			$erros[] = 'E-mail ou senha iválido';
 		}
-
-		if (empty($erros)) {
-			//sem erros
-			//recuperar ID do usuario
-
-			$q = "SELECT id, p_nome FROM usuario WHERE email = '$n' AND pass = SHA1('M@c@c8!ouc832.$s')";
-			$r = mysqli_query($dbc, $q);
-
-			if (mysqli_num_rows($r) == 0)
-			{
-				$row = mysqli_fetch_array($r, MYSQLI_ASSOC);
-
-				return array(true, $row);
-			} 
-			else
-			{
-				$erros = 'Informações não coincidem!';
-			}
-		}
-		return array(false, $erros);
-	}
-?> 
+	  }
+	return array(false, $erros);
+  }
+?>
