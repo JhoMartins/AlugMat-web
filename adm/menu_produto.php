3068de74a8972a955d117225fcb50c245c813d08
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-05-02 22:51:52 -0300

    criaçao a estrutura html a pagina e tabela para os registros com php

--- adm/menu_produto.php
+++ adm/menu_produto.php
@@ -43,10 +43,90 @@
         break;
   }
 
-  $q = "SELECT * FROM produto ORDER BY $order_by LIMIT $inicio, $exiba";
+  $q = "SELECT id, descricao FROM produto ORDER BY $order_by LIMIT $inicio, $exiba";
   $r = @mysqli_query($dbc, $q);
 
   if (mysqli_num_rows($r) > 0) {
-      
+      $saida = '<div class="table-responsive col-md-12">
+      <table class="table table-striped">
+      <thead>
+        <tr>
+          <th width="10%"><strong>
+            <a href="menu_produto?ordem=id">Código</a></strong></th>
+          <th width="10%"><strong>
+            <a href="menu_produto?ordem=d">Descrição</a></strong></th>
+        </tr>
+      </thead> <tbody>';
+
+      while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+          $saida .= '<tr>
+          <td>' . $row['id'] . '</td>
+          <td>' . $row['descricao'] . '</td>
+          <td class="actions">
+            <a href="alt_produto.php?id=' . $row['id'] . '" class="btn btn-xs btn-warning">Editar</a>
+            <a href="exc_produto.php?id=' . $row['id'] . '" class="btn btn-xs btn-danger">Excluir</a>
+          </td>
+          </tr>';
+      }
+      $saida .= '</tbody></table></div>';
+  } else {
+        $saida = "<div class='alert alert-warning'>
+        Sua pesquisa por <strong>$where</strong> 
+        não encontrou nenhum resultado.<br />";
+        $saida .= "<strong>Dicas</strong><br />";
+        $saida .= "- Tente palavras menos específicas<br />";
+        $saida .= "- Tente palavras chaves diferentes<br />";
+        $saida .= "- Confira a ortografia das palavras
+        e se elas foram acentuadas corretamente.<br />";
   }
+?>
+
+<div id="main" class="container-fluid">
+  <div id="top" class="row">
+	
+	<div class="col-md-3">
+	  <h2>Usuários</h2>
+	</div>
+	
+	<div class="col-md-6">
+	  <div class="input-group h2">
+	    <input class="form-control"
+			id="busca"
+			type="text"
+			placeholder="Pesquisa de miniatura por Nome" />
+		<span class="input-group-btn">
+		 <a href="#" 
+		    onclick="this.href='menu_produto.php?q='+
+			   document.getElementById('busca').value"
+		 class="btn btn-primary">
+		<span class="glyphicon glyphicon-search">
+		</span>
+		 </a>
+		</span>
+	  </div>
+	</div>
+	
+	<div class="col-md-3">
+	  <a href="usuario_cad.php" 
+		class="btn btn-primary pull-right h2">
+		Inserir Miniatura</a>
+	</div>
+  </div>
+</div>
+
+<hr />
+
+<div id="list" class="row">
+  <?php echo $saida; ?>
+</div>
+
+<!-- <div id="botton" class="row">
+  <ul class="pagination">
+    <?php if (isset($pag)) {echo $pag;} ?>
+  </ul>
+</div> -->
+  
+
+<?php
+  include_once('../includes/rodape.php');
 ?>
