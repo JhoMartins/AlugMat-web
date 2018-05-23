402f8ee6df7f8fef282557acd1c8bc872258ef2e
crispoletto <cris.poletto@hotmail.com>
2018-05-23 20:50:24 -0300

    Correções Gerais
    - Correção na verificação de e-mail já existente ao alterar um cadastro de usuário.
    
    - Função de Ampliar Imagem na página inicial e categorias.

--- includes/conexao.php
+++ includes/conexao.php
@@ -7,4 +7,6 @@
   define('DB_BANCO','bd_alugmat');
   
   $dbc = @mysqli_connect(DB_SERVIDOR,DB_USUARIO,DB_SENHA,DB_BANCO) or die ('Não foi possível conectar ao MySQL: ' . mysqli_connect_error());
+  
+  mysqli_set_charset($dbc,'uft8');
 ?>
