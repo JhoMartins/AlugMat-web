2e2ca3c04340a6ace571656cc08cbbf6bc1db90b
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-06-03 11:01:05 -0300

    corrige mascara de celular

--- js/masks.js
+++ js/masks.js
@@ -4,13 +4,5 @@ $( document ).ready(function($) {
     $('#ie').mask('000.000.000.000');
     $('#telefone').mask('(00)0000-0000');
     $('#valor_diaria').mask('000000000000000.00', {reverse: true})
-    // Para aplicar a mascara correta dependendo da quantidade de numeros do celular
-    var maskBehavior = function (val) {
-        return val.replace(/\D/g, '').length === 11 ? '(00)00000-0000' : '(00)0000-0000';
-       },
-       options = {onKeyPress: function(val, e, field, options) {
-        field.mask(maskBehavior.apply({}, arguments), options);
-        }
-       };
-       $('#celular').mask(maskBehavior, options);
+    $('#celular').mask('(00)00000-0000');
 });
